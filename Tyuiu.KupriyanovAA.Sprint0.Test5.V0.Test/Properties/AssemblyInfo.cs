33283b46f3544e450201954b17e1e3f7f6b78b03
Алексей Куприянов <Tyuiu.KupriyanovAA@outlook.com>
2026-09-19 18:24:03 +0500

    Task5

---
+++ Tyuiu.KupriyanovAA.Sprint0.Test5.V0.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.KupriyanovAA.Sprint0.Test5.V0.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.KupriyanovAA.Sprint0.Test5.V0.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("81f42c92-c99a-4101-a68a-cdfe5a9a5662")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
